ff6cfaed521edc38a638bf046967d157d1941625
Jake Wilson <hi@jkwlsn.dev>
2026-02-19 10:14:22 +0000

    Config `vite-pwa/astro`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jkwlsn.dev',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    AstroPWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**.*.{js,css,ico,svg,avif,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: { cacheName: 'html-cache' },
+          },
+        ],
+      },
+      devOptions: { enabled: true, navigateFallbackAllowlist: [/^\/$/] },
+    }),
+  ],
   markdown: {
     syntaxHighlight: false,
   },
