71e264ec3cb918a2b64060ecbf44987708d3a93c
Jake Wilson <hi@jkwlsn.dev>
2026-08-07 15:52:36 +0100

    Add fonts to Astro config
    Update Astro config to include font files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import AstroPWA from '@vite-pwa/astro';
 
@@ -27,4 +27,36 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: false,
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'InstrumentSerif',
+      cssVariable: '--font-instrument-serif',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/InstrumentSerif-Regular.woff2'],
+            weight: 'normal',
+            style: 'normal',
+          },
+        ],
+      },
+      fallbacks: ['serif'],
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'IBMPlexMono-Regular',
+      cssVariable: '--font-plex-mono-regular',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/IBMPlexMono-Regular.woff2'],
+            weight: 'normal',
+            style: 'normal',
+          },
+        ],
+      },
+      fallbacks: ['monospace'],
+    },
+  ],
 });
